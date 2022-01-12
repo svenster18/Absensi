7e3c3df93108ae624c8b03986557afa0a49f75a2
svenster18 <mohamadrizki8@gmail.com>
2022-01-12 09:42:41 +0700

    add login activity

---
+++ app/src/main/java/com/mohamadrizki/absensi/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.mohamadrizki.absensi.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
